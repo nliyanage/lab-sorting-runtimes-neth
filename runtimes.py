15ade1ab94bede9f4fffb422067ae67169469ebc
nethmin liyanage <nethmin.liyanage@gmail.com>
2023-03-03 12:14:47 -0800

    fixme is a work in progress oops

--- runtimes.py
+++ runtimes.py
@@ -21,6 +21,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # perform the runtime tests
+    print("| ---- |  `timsort`  |  `merge_sorted`  |  `quick_sorted`  ")
     for x in range(0, args.max_x+1):
 
         if args.input == 'random':
@@ -55,7 +56,7 @@ if __name__ == '__main__':
         # You will have to look up how to do this formatting.
         # In order to get a proper markdown table,
         # you will have to also print a header line somewhere else.
-        print(f'len(xs)=2**{x} runtimes={runtimes}')
+        print(f"| len(xs)=2**{x} | {runtimes['timsort']:0.2e} | {runtimes['merge_sorted']:0.2e} | {runtimes['quick_sorted']:0.2e}")
 
         # HINT:
         # use f-strings and a print statement that looks something like
